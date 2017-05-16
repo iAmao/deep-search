2e8184adbeb663c82392fdacbe18541d96f747a2
Inumidun Amao <inumidun.amao@andela.com>
2017-05-16 14:19:56 +0100

    add travisCI and coveralls coverage to project

--- javascript/tests/DeepSearch.spec.js
+++ javascript/tests/DeepSearch.spec.js
@@ -26,4 +26,19 @@ describe('DeepSearch', () => {
       expect(deepSearch.searchKey('key', {})).to.eql('Input Error');
     });
   });
+
+  describe('validateInput', () => {
+    it('should return true on a valid string input', () => {
+      expect(DeepSearch.validateInput('one', 'string')).to.eql(true);
+    });
+    it('should return false on an invalid string input', () => {
+      expect(DeepSearch.validateInput('', 'string')).to.eql(false);
+    });
+    it('should return true on a valid object input', () => {
+      expect(DeepSearch.validateInput({ one: 'valid' }, 'object')).to.eql(true);
+    });
+    it('should return false on an invalid object input', () => {
+      expect(DeepSearch.validateInput({}, 'object')).to.eql(false);
+    });
+  });
 });
